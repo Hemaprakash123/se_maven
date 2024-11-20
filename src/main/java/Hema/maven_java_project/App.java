ceb63cdf388d60e4ea6a2e9c87ef7647ce4f1372
Hema prakash <hemaprakshreddy73@gmail.com>
2024-11-20 16:04:46 +0530

    Hi bros

---
+++ src/main/java/Hema/maven_java_project/App.java
@@ -0,0 +1,13 @@
+package Hema.maven_java_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
